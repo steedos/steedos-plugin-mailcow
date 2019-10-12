d0602de668adb811855bd26327b27d8d5df94da1
hotlong <zhuangjianguo@hotoa.com>
2019-10-12 17:24:51 +0800

    upgrade core

--- server.js
+++ server.js
@@ -1,26 +1,14 @@
 var server = require('@steedos/meteor-bundle-runner');
-var objectql = require("@steedos/objectql")
-var path = require('path');
+var steedos = require('@steedos/core')
 server.Fiber(function () {
-    server.Profile.run("Server startup", function () {
-        server.loadServerBundles();
-        try {
-            var steedosSchema = objectql.getSteedosSchema()
-            steedosSchema.addDataSource('mailcow', {
-                driver: "mysql",
-                host: "127.0.0.1",
-                port: 13306,
-                username: "mailcow",
-                password: "kSwR8tkHWwPYaMpv06faAw6rNYBP",
-                database: "mailcow",
-                objectFiles: [path.resolve(__dirname, "./src")],
-                appFiles: [path.resolve(__dirname, "./src")]
-            });
-            steedosSchema.getDataSource('mailcow').init();
-        } catch (error) {
-            console.log(error)
-        }
-        server.callStartupHooks();
-        server.runMain();
-    });
-}).run();
+    try {
+        server.Profile.run("Server startup", function () {
+            server.loadServerBundles();
+            steedos.init();
+            server.callStartupHooks();
+            server.runMain();
+        })
+    } catch (error) {
+       console.error(error.stack)
+    }
+}).run()
