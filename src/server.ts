2df64751dd3400765b73afce48c504d3ea9c258e
baozhoutao <baozhoutao@hotoa.com>
2019-10-22 15:31:07 +0800

    添加未读邮件unread_mail

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
 var server = require('@steedos/meteor-bundle-runner');
 var steedos = require('@steedos/core')
+var express = require('express');
+import unread from './message';
+var app = express();
+console.log('unread', unread)
+app.use(unread);
+declare var WebApp;
 server.Fiber(function () {
     try {
         server.Profile.run("Server startup", function () {
             server.loadServerBundles();
+            WebApp.connectHandlers.use(app);
             steedos.init();
             server.callStartupHooks();
             server.runMain();
