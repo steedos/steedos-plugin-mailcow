ba032285328dca022558e016e28f1dcdfa4e4474
chenzhipei <chenzhipei@hotoa.com>
2020-02-03 17:42:04 +0800

    批量导入oa邮件用户

--- migrations/1572939514911-mailbox.js
+++ migrations/1572939514911-mailbox.js
@@ -3,6 +3,35 @@ var objectql = require("@steedos/objectql");
 objectql.getDataSource('mailcow').init();
 var mailbox = objectql.getObject('mailbox');
 var userData = require("../oausers.json");
+
+let insertUsers = async function (userData) {
+  console.log(userData);
+  for (let user of userData) {
+    if (user){
+      let spaceUser = await mailbox.find({
+        filters: [["local_part", "=", user.local_part]]
+      }).catch((ex) => {
+        console.error(ex);
+        return [];
+      });
+      if (spaceUser.length < 1){
+        user.password = user.password.toString();
+        user.quota = parseFloat(user.quota);
+        let insertedDoc = await mailbox.insert(user).catch((ex) => {
+          console.error(ex);
+          return {};
+        });
+        console.log("INSERTED accounts:", insertedDoc.name)
+      }else{
+        console.log("spaceUser exists: ",user.local_part);
+      }
+    }
+  }
+}
 module.exports.up = async function (next) {
-  console.log(await mailbox.find());
+  await insertUsers(userData);
+};
+
+module.exports.down = async function (next) {
+  await insertUsers(userData);
 };
