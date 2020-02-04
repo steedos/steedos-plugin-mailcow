7124576c19e963c0b87dc1cfaf6805e032251e09
chenzhipei <chenzhipei@hotoa.com>
2020-02-04 17:54:34 +0800

    批量更新用户:下次登录修改密码

--- migrations/1572939514911-mailbox.js
+++ migrations/1572939514911-mailbox.js
@@ -5,7 +5,6 @@ var mailbox = objectql.getObject('mailbox');
 var userData = require("../oausers.json");
 
 let insertUsers = async function (userData) {
-  console.log(userData);
   for (let user of userData) {
     if (user){
       let spaceUser = await mailbox.find({
@@ -21,17 +20,41 @@ let insertUsers = async function (userData) {
           console.error(ex);
           return {};
         });
-        console.log("INSERTED accounts:", insertedDoc.name)
+        console.log("INSERTED user:", insertedDoc.name)
       }else{
         console.log("spaceUser exists: ",user.local_part);
       }
     }
   }
 }
+
+let updateUsers = async function () {
+  let upAttributes = `{"force_pw_update": "1", "tls_enforce_in": "0", "tls_enforce_out": "0", "sogo_access": "1", "mailbox_format": "maildir:", "quarantine_notification": "hourly"}`;
+  let users = await mailbox.find({
+    filters: [["domain", "=", "qdrq.com"]]
+  }).catch((ex) => {
+    console.error(ex);
+    return [];
+  });
+  if (users.length > 0){
+    for (let user of users) {
+      let updatedDoc = await mailbox.directUpdate(user.username,{
+        attributes: upAttributes
+      }).catch((ex) => {
+        console.error(ex);
+        return {};
+      });
+      console.log("UPDATED user:", updatedDoc.name);
+    }
+  }
+}
+
 module.exports.up = async function (next) {
-  await insertUsers(userData);
+  // await insertUsers(userData);
+  await updateUsers();
 };
 
 module.exports.down = async function (next) {
-  await insertUsers(userData);
+  // await insertUsers(userData);
+  await updateUsers();
 };
