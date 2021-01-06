1855872524e367d82808454c51fade73120daef1
chenzhipei <chenzhipei@hotoa.com>
2021-01-06 18:12:00 +0800

    修改用户默认接受所有回执请求、默认为文本模式

--- migrations/1572939514911-mailbox.js
+++ migrations/1572939514911-mailbox.js
@@ -2,7 +2,8 @@
 var objectql = require("@steedos/objectql");
 objectql.getDataSource('mailcow').init();
 var mailbox = objectql.getObject('mailbox');
-var userData = require("../oausers.json");
+var profile = objectql.getObject('sogo_user_profile');
+// var userData = require("../oausers.json");
 
 let insertUsers = async function (userData) {
   for (let user of userData) {
@@ -49,12 +50,81 @@ let updateUsers = async function () {
   }
 }
 
+let updateUserProfile = async function(){
+  let mails = await mailbox.find({
+    filters: [["domain", "=", "qdrq.com"]]
+  }).catch((ex) => {
+    console.error(ex);
+    return [];
+  });
+
+  if (mails.length > 0){
+    for (let mail of mails) {
+      let userProfile = await profile.find({
+        filters: [["c_uid", "=", mail.username]]
+      }).catch((ex) => {
+        console.error(ex);
+        return [];
+      });
+      console.log("userProfile.length: ",userProfile.length);
+      if (userProfile.length > 0){
+        let c_default = userProfile[0].c_defaults;
+        console.log('c_default: ',userProfile[0].c_uid);
+        c_default = JSON.parse(c_default);
+        // 默认是文本模式
+        c_default.SOGoMailComposeMessageType = "text";
+        // 默认同意所有回执请求
+        c_default.SOGoMailReceiptAllow = "1";
+        c_default.SOGoMailReceiptOutsideDomainAction = "send";
+        c_default.SOGoMailReceiptAnyAction = "send";
+        c_default.SOGoMailReceiptNonRecipientAction = "send";
+
+        let setting = JSON.stringify(c_default);
+
+        let updatedDoc = await profile.directUpdate(mail.username,{
+          c_defaults: setting
+        }).catch((ex) => {
+          console.error(ex);
+          return {};
+        });
+
+        console.log("UPDATED userProfile:", updatedDoc.c_uid);
+      }else{
+        let new_default = {};
+        let new_profile = {};
+        // 默认是文本模式
+        new_default.SOGoMailComposeMessageType = "text";
+        // 默认同意所有回执请求
+        new_default.SOGoMailReceiptAllow = "1";
+        new_default.SOGoMailReceiptOutsideDomainAction = "send";
+        new_default.SOGoMailReceiptAnyAction = "send";
+        new_default.SOGoMailReceiptNonRecipientAction = "send";
+
+        new_default = JSON.stringify(new_default);
+
+        new_profile.c_uid = mail.username;
+        new_profile.c_defaults = new_default;
+        new_profile.c_settings = `{"Calendar": {}}`;
+
+        // 插入新设置
+        let insertedDoc = await profile.insert(new_profile).catch((ex) => {
+          console.error(ex);
+          return {};
+        });
+        console.log("INSERTED profile:", insertedDoc.c_uid);
+      }
+    }
+  }
+}
+
 module.exports.up = async function (next) {
   // await insertUsers(userData);
-  await updateUsers();
+  // await updateUsers();
+  await updateUserProfile();
 };
 
 module.exports.down = async function (next) {
   // await insertUsers(userData);
-  await updateUsers();
+  // await updateUsers();
+  await updateUserProfile();
 };
